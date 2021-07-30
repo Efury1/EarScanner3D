323775f1a2ed51f3fcca481012352959f36c1ea1
James <james.noye@bigpond.com>
2021-07-30 18:07:29 +1000

    Initial Commit

---
+++ EarScanner_XCode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EarScanner_XCode
+//
+//  Created by James Noye on 30/7/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
