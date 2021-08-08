d3b8339eac942bbe770cea071b875ed00e679268
James <james.noye@bigpond.com>
2021-08-08 18:48:42 +1000

    Built out the login text to code to API

--- EarScanner_XCode/ViewControllers/LoginViewController.swift
+++ EarScanner_XCode/ViewControllers/LoginViewController.swift
@@ -11,9 +11,15 @@ import UIKit
 
 class LoginViewController: UIViewController {
     
-    @IBOutlet weak var
-        emailLoginTextField:
-            UITextField!
+ 
+    
+    
+    
+    //gets text from emailLoginTextField and saves it to email
+//    @IBAction func getVal () {
+//        var email: String = emailLoginTextField.text!
+//
+//    }
     
     
     @IBOutlet weak var
@@ -29,17 +35,27 @@ class LoginViewController: UIViewController {
         super.viewDidLoad()
         //Additional setup after loading view
     }
+    //gets the text inputs
+    @IBOutlet weak var PasswordField: UITextField!
     
+    @IBOutlet weak var EmailField: UITextField!
     //Handing Login button
     @IBAction func
     loginTapped(_ sender:
                     Any) {
+//    saves the text inputs to variables
+     guard
+        let email = EmailField.text,
+        let password = PasswordField.text
+     else {return}
+        
         //set the url of the api
-        let url = URL(string:  "https://r316dbbv9l.execute-api.ap-southeast-2.amazonaws.com/Version2-POST")
+        let url = URL(string:  "https://r316dbbv9l.execute-api.ap-southeast-2.amazonaws.com/Version2-POST/login")
         //make a request object with the url
         var request = URLRequest(url: url!)
         //attach the json body to he request
-        let jsonbody = [  "UserName": "James", "Password": "Noye"]
+        //pass in the text inputs
+        let jsonbody = [  "Email": email, "Password": password]
         //making sure to convet it to json and attach it, testing if it breaks
         do { let requestBody = try JSONSerialization.data(withJSONObject: jsonbody, options: .fragmentsAllowed)
             request.httpBody = requestBody
@@ -55,13 +71,24 @@ class LoginViewController: UIViewController {
         let dataTask = session.dataTask(with: request) { data, response, error in
         //decode the request and print the result
             let UserExists = String(decoding: data!, as: UTF8.self)
+            /// USEREXISTS
             print(UserExists) //returns true or false boolean
-            
+            ///
 
         }
         //resume the datatask
         dataTask.resume()
         
+        
+    }
+}
+extension LoginViewController : UITextFieldDelegate {
+        
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        
+        return true
+    }
         
     }
     
@@ -74,8 +101,7 @@ class LoginViewController: UIViewController {
             
      
     
-        }
-    
+        
 
 
 
