6cd58dfee5b4f1fca70444c17f00a0205814bfe3
Eliza Fury <Elizajadefury@gmail.com>
2021-08-15 16:33:38 +1000

    Pop up box when you press login and your password is wrong

--- EarScanner_XCode/ViewControllers/LoginViewController.swift
+++ EarScanner_XCode/ViewControllers/LoginViewController.swift
@@ -134,7 +134,17 @@ class LoginViewController: UIViewController {
         }
         else{
             ///SAY WRONG EMAIL OR PASSWORD
-            print("wrong email or password")
+            //print("wrong email or password")
+            
+            // create the alert
+            let alert = UIAlertController(title: "My Title", message: "This is my message.", preferredStyle: UIAlertController.Style.alert)
+
+            // add an action (button)
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+
+            // show the alert
+            self.present(alert, animated: true, completion: nil)
+            
         }
         
            
