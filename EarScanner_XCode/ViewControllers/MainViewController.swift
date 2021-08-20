047d560a2c1f355e2fed47148c6496f8ce726390
Isabella Turco <igturco@hotmail.com>
2021-08-20 13:27:55 +1000

    for Eliza to look at

--- EarScanner_XCode/ViewControllers/MainViewController.swift
+++ EarScanner_XCode/ViewControllers/MainViewController.swift
@@ -8,9 +8,18 @@
 import Foundation
 import UIKit
 
-class MainViewController: UINavigationController {
-
-    fileprivate func isLoggedIn() -> Bool {
+class MainViewController: UIViewController {
+    
+    override func
+    viewDidLoad() {
+        super.viewDidLoad()
+        self.hideKeyboardWhenTappedAround()
+        //Additional setup after loading view
+    }
+    
+    
+    
+    /*fileprivate func isLoggedIn() -> Bool {
         return UserDefaults.standard.bool(forKey: "isLoggedin")
     }
     
@@ -18,4 +27,17 @@ class MainViewController: UINavigationController {
         UserDefaults.standard.set(false, forKey: "isLoggedIn")
         UserDefaults.standard.synchronize()
     }
+    
+    func funalert() {
+        let alert = UIAlertController(title: "Error", message: "Please Check Internet Connection", preferredStyle: UIAlertController.Style.alert) //create alert
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)) // add an action (button)
+        self.present(alert, animated: true, completion: nil)
+    }*/
+    
+    @IBAction func logoutButton(_ sender: Any) {
+        let alert1 = UIAlertController(title: "Error", message: "Connection Timed Out", preferredStyle: UIAlertController.Style.alert) //create alert
+        alert1.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)) // add an action (button)
+         self.present(alert1, animated: true, completion: nil) // show the alert
+    }
+    
 }
