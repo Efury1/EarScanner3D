a20e0a29a04222be889ae48ae1c84fd9f06e0423
Eliza Fury <Elizajadefury@gmail.com>
2021-08-22 20:46:03 +1000

    ForgotPassViewController

---
+++ EarScanner_XCode/ViewControllers/ForgotPassViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ForgotPassViewController.swift
+//  EarScanner_XCode
+//
+//  Created by Eliza Fury on 22/8/21.
+//
+
+import SwiftUI
+import Foundation
+import UIKit
+
+class ForgotPassViewController: UIViewController {
+    
+    @IBOutlet weak var email: UITextField!
+    @IBAction func sendEmail(_ sender: Any) {
+       //Need to confirm email exisits
+        
+    }
+        
+    
+}
+
--- EarScanner_XCode/ViewControllers/SignupViewController.swift
+++ EarScanner_XCode/ViewControllers/SignupViewController.swift
@@ -30,50 +30,7 @@ class SignUpViewController: UIViewController, UITextFieldDelegate {
     //delegate allows text field to be a string
     override func viewDidLoad() {
         super.viewDidLoad()
-        emailTextField.delegate = self
-        passwordTextField.delegate = self
-    }
-    //create encyption key for passoword
-    func passwordKey(_ passwordTextField: String) -> SymmetricKey {
-      // Create a SHA256 hash from the provided password
-      let hash = SHA256.hash(data: passwordTextField.data(using: .utf8)!)
-      // Convert the SHA256 to a string. This will be a 64 byte string
-      let hashString = hash.map { String(format: "%02hhx", $0) }.joined()
-      // Convert to 32 bytes
-      let subString = String(hashString.prefix(32))
-      // Convert the substring to data
-      let keyData = subString.data(using: .utf8)!
-     
-      // Create the key use keyData as the seed
-      return SymmetricKey(data: keyData)
-    }
-    
-    //create encyption key for email
-    func emailKey(_ emailTextField: String) -> SymmetricKey {
-      // Create a SHA256 hash from the provided password
-      let hash = SHA256.hash(data: emailTextField.data(using: .utf8)!)
-      // Convert the SHA256 to a string. This will be a 64 byte string
-      let hashString = hash.map { String(format: "%02hhx", $0) }.joined()
-      // Convert to 32 bytes
-      let subString = String(hashString.prefix(32))
-      // Convert the substring to data
-      let keyData = subString.data(using: .utf8)!
-     
-      // Create the key use keyData as the seed
-      return SymmetricKey(data: keyData)
-    }
-    
-    func encryptCodableObject<T: Codable>(_ object: T, usingKey key: SymmetricKey) throws -> String {
-      // Convert to JSON in a Data record
-      let encoder = JSONEncoder()
-      let userData = try encoder.encode(object)
-     
-      // Encrypt the userData
-      let encryptedData = try ChaChaPoly.seal(userData, using: key)
-     
-      // Convert the encryptedData to a base64 string which is the
-      // format that it can be transported in
-      return encryptedData.combined.base64EncodedString()
+      
     }
     
  
@@ -89,82 +46,84 @@ class SignUpViewController: UIViewController, UITextFieldDelegate {
 
     let email = emailTextField.text!
     let password = passwordTextField.text!
-    let jsonbody = [  "Email": email, "Password": password] as [String : Any]
-        
-        
-        do {
-            let requestBody = try JSONSerialization.data(withJSONObject: jsonbody, options: .fragmentsAllowed)
-            request.httpBody = requestBody
-        } catch let error {
-            print(error.localizedDescription)
-        }
-    
-
-        //Create dataTask using the session object to send to server
-    var UserExists = "False"
-    
-    let dataTask =  session.dataTask(with: request) { data, response, error in
-        print("heh2")
-    //decode the request and print the result
-        //checks if response returned and if not checks internet
-        if (data != nil){
-    UserExists = String(decoding: data!, as: UTF8.self)
-        /// USEREXISTS
-        
-        print(UserExists)
-        
-        if (UserExists.contains("True")){
-            //Implement autologin
-            self.userDefaults.setValue(true, forKey: "UserExists")
-            DispatchQueue.main.async {
-//                    self.performSegue(withIdentifier: "loginSegue", sender: self)
-                let childViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "StartMain")
-                 self.addChild(childViewController)
-                 self.view.addSubview(childViewController.view)
-                 childViewController.didMove(toParent: self)
-                
-            }
-            
-            
-            return
-        }
-        else if (UserExists.contains("False"))
-        {
-            DispatchQueue.main.async {
-            print("alert")
-            let alert = UIAlertController(title: "Error", message: "Something Went Wrong", preferredStyle: UIAlertController.Style.alert) //create alert
-            alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)) // add an action (button)
-            self.present(alert, animated: true, completion: nil) // show the alert
-            }
-            
-            
-            return
-        }
-        }
-        else{
-            print("error with connection")
-            DispatchQueue.main.async {
-            print("alert")
-            let alert = UIAlertController(title: "Error", message: "Please Check Internet Connection", preferredStyle: UIAlertController.Style.alert) //create alert
-                //I'm a pop up
+    if((email == "") || password == "") {
+                print("Please ensure all fields are typed in")
+                DispatchQueue.main.async {
+                print("alert")
+                let alert = UIAlertController(title: "Error", message: "Please make sure you fill in all fields", preferredStyle: UIAlertController.Style.alert) //create alert
                 alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)) // add an action (button)
-            self.present(alert, animated: true, completion: nil)
-                //I end here
-                // show the alert
+                self.present(alert, animated: true, completion: nil) // show the alert
+                }
+        
+            }
+            else {
+        let jsonbody = [  "Email": email, "Password": password] as [String : Any]
+            
+            
+            do {
+                let requestBody = try JSONSerialization.data(withJSONObject: jsonbody, options: .fragmentsAllowed)
+                request.httpBody = requestBody
+            } catch let error {
+                print(error.localizedDescription)
+            }
+        
+
+            //Create dataTask using the session object to send to server
+        var UserExists = "False"
+        
+        let dataTask =  session.dataTask(with: request) { data, response, error in
+            print("heh2")
+        //decode the request and print the result
+            //checks if response returned and if not checks internet
+            if (data != nil){
+        UserExists = String(decoding: data!, as: UTF8.self)
+            /// USEREXISTS
+            
+            print(UserExists)
+            
+            if (UserExists.contains("True")){
+                //Implement autologin
+                self.userDefaults.setValue(true, forKey: "UserExists")
+                DispatchQueue.main.async {
+
+                    let childViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "StartMain")
+                     self.addChild(childViewController)
+                     self.view.addSubview(childViewController.view)
+                     childViewController.didMove(toParent: self)
+                    
+                }
+                
+                
+                return
+            }
+            else if (UserExists.contains("False"))
+            {
+                DispatchQueue.main.async {
+                print("alert")
+                let alert = UIAlertController(title: "Error", message: "Something Went Wrong", preferredStyle: UIAlertController.Style.alert) //create alert
+                alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)) // add an action (button)
+                self.present(alert, animated: true, completion: nil) // show the alert
+                }
+                
+                
+                return
+            }
+            }
+            else{
+                print("error with connection")
+                DispatchQueue.main.async {
+                print("alert")
+                let alert = UIAlertController(title: "Error", message: "Please Check Internet Connection", preferredStyle: UIAlertController.Style.alert) //create alert
+                    //I'm a pop up
+                    alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)) // add an action (button)
+                self.present(alert, animated: true, completion: nil)
+                    //I end here
+                    // show the alert
+                }
             }
         }
-        
-         //returns true or false string
-        
-    }
-//            do { //Json object from data
-//                if let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String: Any] {
-//                    print(json)
-//                }
-//            } catch let error {
-//                print(error.localizedDescription)
-//            }
-        
-        dataTask.resume()
+            dataTask.resume()
+        }
     }
 }
+
