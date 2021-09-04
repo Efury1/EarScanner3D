952e4006711cc5f16756505a786dcd1d09e1220a
Eliza Fury <Elizajadefury@gmail.com>
2021-09-04 13:37:33 +1000

    Page for email code done. Can't get space between the top and bottom

--- EarScanner_XCode/Camera/GridView.swift
+++ EarScanner_XCode/Camera/GridView.swift
@@ -1,8 +1,10 @@
 import AVFoundation
 import UIKit
 import Foundation
+import SwiftUI
 class GridView: UIView {
     
+
 // Only override draw() if you perform custom drawing.
 // An empty implementation adversely affects performance during animation.
 override func draw(_ rect: CGRect) {
