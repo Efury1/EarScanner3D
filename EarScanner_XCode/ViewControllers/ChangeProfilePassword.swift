60e64e611b277767465241324201032c73e0e8dd
James <james.noye@bigpond.com>
2021-10-16 19:45:10 +1000

    Userprofile branch added to this one

--- EarScanner_XCode/ViewControllers/ChangeProfilePassword.swift
+++ EarScanner_XCode/ViewControllers/ChangeProfilePassword.swift
@@ -4,7 +4,6 @@
 //
 //  Created by James Noye on 24/9/21.
 //
-
 import Foundation
 import SwiftUI
 
@@ -19,6 +18,7 @@ class ChangePasswordViewController: UIViewController {
     @IBAction func ChangePasswordProfile(_ sender: UIButton) {
         
         let OldPasswordText = OldPassword.text;
+        
         let NewPasswordText = NewPassword.text;
         let ConfirmPasswordText = ConfirmPassword.text;
         
@@ -28,7 +28,8 @@ class ChangePasswordViewController: UIViewController {
 
             var request = URLRequest(url: url!) //make a request object with the url
            
-            let jsonbody = [ "NewPassword": NewPasswordText!, "OldPassword": cryto(password: OldPasswordText!), "Email": LoginViewController.Email] //attach the json body to he request. pass in the text inputs
+            let jsonbody = [ "NewPassword": cryto(password: NewPasswordText!), "OldPassword": cryto(password: OldPasswordText!), "Email": LoginViewController.Email] //attach the json body to he request. pass in the text inputs
+            
             print("testing password", NewPasswordText!, OldPasswordText!, LoginViewController.Email)
             do //making sure to convet it to json and attach it, testing if it breaks
             {
@@ -44,39 +45,54 @@ class ChangePasswordViewController: UIViewController {
             var APIResponse = "False"
             
             //make the request
-
-            
             let dataTask =  session.dataTask(with: request) { data, response, error in
-            //decode the request and print the result
-                //checks if response returned and if not checks internet
-                if (data != nil){
-                    APIResponse = String(decoding: data!, as: UTF8.self)}
-                print("UserExist: ")
-                print(APIResponse)
-        }
-            dataTask.resume()
-            if (APIResponse == "True"){
-            let childViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "StartMain")
+                //decode the request and print the result
+                    //checks if response returned and if not checks internet
+                    if (data != nil){
+                        APIResponse = String(decoding: data!, as: UTF8.self)}
+                    print("UserExist: ")
+                    print(APIResponse)
+                
+            if (APIResponse.contains("True")){
+                DispatchQueue.main.async {
+                let childViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "StartMain")
+               
+                childViewController.modalPresentationStyle = .fullScreen //or .overFullScreen for transparency
+                self.present(childViewController, animated: true, completion: nil)
            
-            childViewController.modalPresentationStyle = .fullScreen //or .overFullScreen for transparency
-            self.present(childViewController, animated: true, completion: nil)
-       
-        //if the passwords are the same hit the API to change the password where the email is this one
+            //if the passwords are the same hit the API to change the password where the email is this one
+                }
             }
-            else if (APIResponse == "False"){
-                let alert = UIAlertController(title: "Incorrect Password", message: "The current password is incorrect", preferredStyle: UIAlertController.Style.alert) //create alert
-                                        //I'm a pop up
-                    alert.addAction(UIAlertAction(title: "Try again", style: UIAlertAction.Style.default, handler: nil)) // add an action (button)
-                    self.present(alert, animated: true, completion: nil)
+            else if (APIResponse.contains("False")){
+                DispatchQueue.main.async {
+                    let alert = UIAlertController(title: "Error", message: "Old Password Doesnt Exist in System", preferredStyle: UIAlertController.Style.alert) //create alert
+                                            //I'm a pop up
+                        alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)) // add an action (button)
+                        self.present(alert, animated: true, completion: nil)
+                }
             }
-    }
-    else{
         
-        let alert = UIAlertController(title: "Invalid Password", message: "The passwords do not match or are invalid. Passwords must contain at least 8 characters and 1 number.", preferredStyle: UIAlertController.Style.alert) //create alert
-                                //I'm a pop up
-            alert.addAction(UIAlertAction(title: "Try again", style: UIAlertAction.Style.default, handler: nil)) // add an action (button)
-            self.present(alert, animated: true, completion: nil)
+        else{
+            DispatchQueue.main.async {
+            let alert = UIAlertController(title: "Error", message: "An Error Has Occured", preferredStyle: UIAlertController.Style.alert) //create alert
+                                    //I'm a pop up
+                alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)) // add an action (button)
+                self.present(alert, animated: true, completion: nil)
+            }
         }
+            }
+            dataTask.resume()
+        }
+        else{
+            DispatchQueue.main.async {
+            let alert = UIAlertController(title: "Error", message: "Passwords Don't Match", preferredStyle: UIAlertController.Style.alert) //create alert
+                                    //I'm a pop up
+                alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)) // add an action (button)
+                self.present(alert, animated: true, completion: nil)
+            }}
     }
     
+    
 }
+    
+    
